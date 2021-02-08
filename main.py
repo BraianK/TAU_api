a7fe3595eb9841d9e5b229156b60a20915f8cad3
BraianK <s16723@pjwstk.edu.pl>
2021-02-08 20:17:07 +0100

    obsluga coinpaprica

--- main.py
+++ main.py
@@ -1,10 +1,34 @@
 import requests
 import json
-
+#link do api https://api.coinpaprika.com/#tag/Coins/paths/~1coins/get
 coin = 'btc-bitcoin'
 url = "https://api.coinpaprika.com/v1/coins/{}".format(coin)
 head = {'content-type': 'application/json'}
 response = requests.request("GET", url)
 a = json.loads(response.text)
+#Wyświetlenie items
+print(a.items())
+# -----------------------------------
+# sprawdzanie kluczy
+expected = ['id', 'name', 'symbol', 'rank']
+current = []
 
-print(a)
+for key, value in a.items():
+    if key in expected:
+        current.append(key)
+assert expected == current
+#Wyświetlenie wartości oczekiwanej i aktualnej
+print(expected)
+print(current)
+# -----------------------------------
+# sprawdzanie odpowiedzi
+assert response.status_code == 200
+#Wyświetlenie odpowiedzi
+print(response.status_code)
+# -----------------------------------
+# sprawdzanie niepoprawnego zapytania
+url = "https://api.coinpaprika.com/v1/acoins/{}".format(coin)  # zmiana endpointu z cases na a
+response = requests.request("GET", url)
+assert response.status_code == 404
+#Wyświetlenie odpowiedzi
+print(response.status_code)
